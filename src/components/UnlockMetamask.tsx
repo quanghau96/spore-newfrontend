56faf13e992b514d9a5d3fc89ec9d729182267c1
cahyohayo <nnugroho51@gmail.com>
2021-06-05 17:02:22 +0700

    upload

---
+++ src/components/UnlockMetamask.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const UnlockMetamask = (props: any) => {
+  return (
+    <section className="bg-grey" >
+      <div className="container align-items-center d-flex justify-content-center" >
+
+        <div className="text-center" >
+          <p className="alert alert-danger  py-3 px-5" > {props.message} </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default UnlockMetamask;
